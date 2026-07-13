93dc85e47e8a8a1d994b43572415494e573fb0b6
Komala-L <lk0106687@gmail.com>
2026-07-13 21:31:08 +0530

    Configure Express middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,15 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
+
+app.use(cookieParser());
 app.use(express.json());
+
 export default app;
